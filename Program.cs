355658c7194d03f68b4569fd5a4ed305394df498
Ke1ha <Ke1ha@KE1HA>
2023-09-30 11:12:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число");
+long a = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Введите число");
+long b = Convert.ToInt64(Console.ReadLine());
+string c;
+Console.WriteLine("Введите знак");
+c = Console.ReadLine();
+if (b == 0 && c == "/")
+{
+    Console.WriteLine("Деление на ноль невозможно");
+}
+else
+{
+    switch (c)
+    {
+        case "+":
+            Console.Write(a + " " + c + " " + b + " " + "=" + " " + (a + b));
+            break;
+        case "-":
+            Console.Write(a + " " + c + " " + b + " " + "=" + " " + (a - b));
+            break;
+        case "*":
+            Console.Write(a + " " + c + " " + b + " " + "=" + " " + (a * b));
+            break;
+        case "/":
+            Console.Write(a + " " + c + " " + b + " " + "=" + " " + (a / b));
+            break;
+        default:
+            Console.WriteLine("ОШИБКА Доступно только + - * /");
+            break;
+    }
+  
+}
+Console.ReadKey();
